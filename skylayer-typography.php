4dc086c52a2ce43e77371a8a16973351bfca0fec
Daniel Slyman <github@skylayer.eu>
2019-10-18 17:47:13 +0200

    Update skylayer-typography.php

--- skylayer-typography.php
+++ skylayer-typography.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Skylayer Typography
  * Plugin URI:        https://skylayer.de
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Daniel Slyman (Skylayer)
  * Author URI:        https://danielslyman.com
  * License:           GPL-2.0+
